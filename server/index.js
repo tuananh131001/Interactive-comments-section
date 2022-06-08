68dc4158492ebd2f05da90e25d6d58cb7f1fd928
William <s3864077@rmit.edu.vn>
2022-06-08 22:25:05 +0700

    try to fix heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.port || 5002;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(
